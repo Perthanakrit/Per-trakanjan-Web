b6c6bcbc065a5fc537457afe74a222bebc876358
thanakrit trakanjan <aupper094@gmail.com>
2022-12-16 17:05:46 +0700

    Update1612

---
+++ MYWebDev/Pages/InfoAchievements/ReadmoreProject1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MYWebDev.Pages.InfoAchievements
+{
+    public class ReadmoreProject1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
