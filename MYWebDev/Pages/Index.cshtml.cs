632f18e1812eec86c20f772d8a50abdff5ffea02
thanakrit trakanjan <aupper094@gmail.com>
2023-01-11 18:47:20 +0700

    11/1/66

--- MYWebDev/Pages/Index.cshtml.cs
+++ MYWebDev/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MYWebDev.Data;
 using MYWebDev.Model;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -9,6 +10,8 @@ namespace MYWebDev.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        private readonly CommentDbContext _context;
+
         public List<Projects> fakeProjectDB = new List<Projects>()
         {
             
@@ -16,17 +19,33 @@ namespace MYWebDev.Pages
             new Projects(){Title ="I AM HAMSTER", ImageUrl ="image_phy.png", ReadmoreUrl="ReadmoreProject3", Dev="Unity C#"},
             new Projects(){Title ="Gadget for a Parkison’s Disease Screening Household", ImageUrl ="codekathon.jpg", ReadmoreUrl="ReadmoreProject1", Dev="Python ML"}
         };
-        
 
-
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, CommentDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
+
+        public List<CommentModel> Comments { get; set; }
 
         public void OnGet()
         {
 
+        }
+  
+        public void OnPost()
+        {
+            CommentModel newComment = new()
+            {
+                Name = Request.Form["Name"],
+                Comment = Request.Form["Comment"]
+            };
+
+            //DataBase
+            _context.comments.Add(newComment);
+            _context.SaveChanges();
+
+            Comments = _context.comments.ToList();
         }
     }
 }
